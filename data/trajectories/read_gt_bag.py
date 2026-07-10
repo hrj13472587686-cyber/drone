17ef6cff27221788c383e9e797110466e07c91ae
hrj <15000980443@163.com>
2026-07-10 12:26:19 +0800

    Changes

--- data/trajectories/read_gt_bag.py
+++ data/trajectories/read_gt_bag.py
@@ -19,7 +19,7 @@ def parse_bag_to_csv(bag_path: str, target_topic: str, csv_save: str):
         with open(csv_save, "w", newline="", encoding="utf-8") as fp:
             writer = csv.writer(fp)
             # PointStamped 只有时间+xyz，无姿态
-            writer.writerow(["time_sec", "x", "y", "z"])
+            writer.writerow(["x", "y", "z","time_sec"])
             counter = 0
 
             for conn, timestamp_ns, raw_bytes in reader.messages(connections=selected_conns):
@@ -29,7 +29,7 @@ def parse_bag_to_csv(bag_path: str, target_topic: str, csv_save: str):
                 pos = msg.point
                 t = timestamp_ns / 1e9
 
-                writer.writerow([t, pos.x, pos.y, pos.z])
+                writer.writerow([ pos.x, pos.y, pos.z, t ])
                 counter += 1
 
     print(f"处理完成，一共 {counter} 条数据，输出：{csv_save}")
@@ -37,8 +37,8 @@ def parse_bag_to_csv(bag_path: str, target_topic: str, csv_save: str):
 
 if __name__ == "__main__":
     # =========修正话题名称=========
-    BAG_PATH = (r"C:\Users\86134\Desktop\drone\data\raw\mmaud\v1_rooftop_simple\dji_mavic3\2023-08-24-11-14-40_mavic3_gt.bag")
-    TARGET_TOPIC = "/leica/point/absolute"
+    BAG_PATH = r'C:\Users\86134\Desktop\drone\data\raw\mmaud\v1_rooftop_simple\dji_mavic3\2023-08-24-11-14-40_mavic3_gt.bag'
+    TARGET_TOPIC = "/leica/point/relative"
     OUTPUT_CSV = "leica_groundtruth.csv"
 
     parse_bag_to_csv(BAG_PATH, TARGET_TOPIC, OUTPUT_CSV)
